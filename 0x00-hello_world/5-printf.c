8eafd549a01dac9b17ddf8df945f22c8c2d58678
karira-1 <wandithiamonik@gmail.com>
2023-01-02 00:10:40 -0800

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#imclude <stdio.h>
+#include <stdio.h>
 /**
  * main - main block
  * Return: 0
  * /
 int main(void)
 {
-print("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
